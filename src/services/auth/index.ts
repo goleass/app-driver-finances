154ea926cabe5e57917ace0cb3183c6ccbb0652e
goleass <leonardo-assuncao1@hotmail.com>
2024-04-23 22:13:24 -0300

    Test 3

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -5,26 +5,6 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '../database'
 // import { createStripeCustomer } from '../stripe'
 
-import { PrismaClient } from '@prisma/client'
-import { Adapter } from 'next-auth/adapters'
-
-export function CustomPrismaAdapter(p: PrismaClient): Adapter {
-  const origin = PrismaAdapter(p)
-  return {
-    ...origin,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    deleteSession: async (sessionToken: any) => {
-      try {
-        console.log('Deleting', { sessionToken })
-        return await p.session.delete({ where: { sessionToken } })
-      } catch (e) {
-        console.error('Failed to delete session', e)
-        return null
-      }
-    },
-  } as unknown as Adapter
-}
-
 export const {
   handlers: { GET, POST },
   auth,
@@ -36,7 +16,7 @@ export const {
     verifyRequest: '/auth',
     newUser: '/app',
   },
-  adapter: CustomPrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
@@ -52,10 +32,7 @@ export const {
         const { url } = params
         const { host } = new URL(url)
 
-        const escapedHost = host.replace(/\./g, '&#8203;.')
-
         console.log(`Sign in to ${host}\n${url}\n\n`)
-        console.log(escapedHost)
       },
     }),
   ],
