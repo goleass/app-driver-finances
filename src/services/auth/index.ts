5f77708e3d876158707aa8e1070c17fbb99c49b2
goleass <leonardo-assuncao1@hotmail.com>
2024-04-23 21:53:41 -0300

    test 2

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -5,6 +5,25 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '../database'
 // import { createStripeCustomer } from '../stripe'
 
+import { PrismaClient } from '@prisma/client'
+import { Adapter } from 'next-auth/adapters'
+
+export function CustomPrismaAdapter(p: PrismaClient): Adapter {
+  const origin = PrismaAdapter(p)
+  return {
+    ...origin,
+    deleteSession: async (sessionToken: any) => {
+      try {
+        console.log('Deleting', { sessionToken })
+        return await p.session.delete({ where: { sessionToken } })
+      } catch (e) {
+        console.error('Failed to delete session', e)
+        return null
+      }
+    },
+  } as unknown as Adapter
+}
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -16,7 +35,7 @@ export const {
     verifyRequest: '/auth',
     newUser: '/app',
   },
-  adapter: PrismaAdapter(prisma),
+  adapter: CustomPrismaAdapter(prisma),
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
