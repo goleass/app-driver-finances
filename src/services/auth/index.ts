cf6b77d660f422f29bdd230488ded13f97f0c396
Leonardo Assunção <leonardoassuncao@deltaglobal.com.br>
2024-07-02 17:47:38 -0300

    Fix

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -19,8 +19,14 @@ export const {
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
     }),
   ],
   // events: {
