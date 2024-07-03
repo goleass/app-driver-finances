b7bcb7f2ec98977aa57e0b1abe0c4da2b41e075b
Leonardo Assunção <leonardoassuncao@deltaglobal.com.br>
2024-07-03 10:43:33 -0300

    log

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,9 @@ import { getUrl } from './lib/get-url'
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('authjs.session-token')
 
+  
   const pathname = request.nextUrl.pathname
+  console.log('token', token, pathname)
 
   if (pathname === '/auth' && token) {
     return NextResponse.redirect(new URL(getUrl('/app')))
